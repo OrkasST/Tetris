323c557863a72c0888ad652e61f9f5b87662aaa4
Егор Каскевич <->
2025-11-29 04:18:20 +0100

    Onscreen controls are implemented

--- inputHandler.js
+++ inputHandler.js
@@ -5,6 +5,14 @@ export class InputHandler {
         this.eventListener = document.addEventListener("keypress", (e) => {
             this.input = e.code
         })
+
+        this.leftButton = document.getElementById("left")
+        this.rightButton = document.getElementById("right")
+        this.rotateButton = document.getElementById("rotate")
+
+        this.leftButton.addEventListener("click", () => this.input = "KeyA")
+        this.rightButton.addEventListener("click", () => this.input = "KeyD")
+        this.rotateButton.addEventListener("click", () => this.input = "KeyR")
     }
     reset() {
         this.input = ''
